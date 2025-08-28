aafdb7ff7b0c54c3443f3a286bb04c49f78a9d52
Nurkadur Aiganym <ajganymnrkadyr@gmail.com>
2025-08-28 23:14:30 +0500

    newwww api

--- routes/gemini.js
+++ routes/gemini.js
@@ -9,7 +9,7 @@ router.post("/ask", async (req, res) => {
 
   try {
     const otinish = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
